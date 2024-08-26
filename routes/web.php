d428d240fc2745f1bf2655f202f18bf25a043a91
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-08-26 17:46:06 +0300

    created a new view for creating a job

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,20 @@ Route::get('/jobs', function () {
 
     $jobs = Job::with('employer')->simplePaginate(3);
 
-    return view("jobs", [
+    return view("jobs.index", [
         'jobs' => $jobs
     ]);
 });
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
 
     $job = job::find($id);
 
-    return view("job", ['job' => $job]);
+    return view("jobs.show", ['job' => $job]);
 });
 
 Route::get("/contact", function () {
