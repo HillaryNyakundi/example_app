c3d9f653b9235d9bf0bd501a0fb9ccc6da1bce89
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-08-27 12:03:44 +0300

    CRUD operations

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,8 +2,12 @@
   <x-slot:heading>
       job Page
   </x-slot:heading>
-  <h1 class="font-bold text-lg">{{ $job['title'] }}</h1>
+  <h1 class="font-bold text-lg">{{ $job->title }}</h1>
   <p>
-    This job pays {{ $job['salary'] }} per year.
+    This job pays {{ $job->salary }} per year.
+  </p>
+
+  <p class="pt-6">
+    <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
   </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/home', function () {
     return view('home');
 });
 
+//index, displays all the jobs
 Route::get('/jobs', function () {
 
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
@@ -17,10 +18,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//show
 Route::get('/jobs/{id}', function ($id) {
 
     $job = job::find($id);
@@ -28,6 +31,7 @@ Route::get('/jobs/{id}', function ($id) {
     return view("jobs.show", ['job' => $job]);
 });
 
+//store or persists a new job in the database
 Route::post('/jobs', function () {
     //validation
     request()->validate([
@@ -41,6 +45,42 @@ Route::post('/jobs', function () {
         'employer_id' => '1',
     ]);
 
+    return redirect('/jobs');
+});
+
+//edit
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    $job = job::find($id);
+
+    return view("jobs.edit", ['job' => $job]);
+});
+
+//update
+Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    //authorize(on hold ...)
+    //update the job
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    //and persist
+    //redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+//Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize
+    //delete the job
+    Job::findOrFail($id)->delete();
+    //redirect
     return redirect('/jobs');
 });
 
